1c0309ae412a5ecb5f55e3a4a9b94d3ba8256c55
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-09-19 02:55:14 +0600

    recipe image api

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework.authentication import TokenAuthentication
 
 from rest_framework import permissions
@@ -41,3 +43,9 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
--- app/core/models.py
+++ app/core/models.py
@@ -2,15 +2,23 @@
 Database models.
 """
 # PYTHON IMPORTS
-from sys import _getframe
+import uuid
+import os
+
 # DJANGO IMPORTS
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, PermissionsMixin
 )
-from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+    return os.path.join('uploads', 'recipe', filename)
 
 
 class UserManager(BaseUserManager):
@@ -85,6 +93,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
